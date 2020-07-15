2d9a77a8ccdfe819852f23e71c0c62a6ba3fe027
XiaoLin <i@xiaolin.in>
2020-07-15 20:19:42 +0800

    chore(rm): remove old

--- modules/config.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const CONFIG = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
-if (!CONFIG) process.exit(1);
-
-module.exports = CONFIG;
--- modules/logger.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = require(__dirname + '/config');
-const log4js = require('log4js');
-
-module.exports = (name) => {
-    const logger = log4js.getLogger(name);
-    logger.level = CONFIG['logLevel'];
-    return logger;
-}
